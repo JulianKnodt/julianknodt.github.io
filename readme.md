fe2a0dcc0b3282fde4f5439cc4f7578a756a3ec3
Julian Knodt <julianknodt@gmail.com>
2025-03-19 23:49:21 -0700

    Add timeline of youtube videos

--- readme.md
+++ readme.md
@@ -194,6 +194,7 @@ years, so I hope that while listening to it, you can experience some of what I'v
 
 - [Music!](playlist.md)
 - [Non-YT Music](other_music.md)
+- [Music Timeline](yt_timeline.html)
 
 ![Site Counter](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fjulianknodt.github.io&count_bg=%2379C83D&title_bg=%23144FDF&icon=&icon_color=%23E7E7E7&title=Visitors&edge_flat=false)
 
--- scripts/yt_timeline.py
+++ scripts/yt_timeline.py
@@ -2,6 +2,7 @@ import json
 from argparse import ArgumentParser
 import subprocess
 import os
+from datetime import datetime, UTC
 
 import yt_dlp
 
@@ -13,6 +14,7 @@ def arguments():
     help="Stages to run"
   )
   a.add_argument("--cache-file", default="cache.json")
+  a.add_argument("--markwhen", default="tmp.mw", help="Store a markwhen file somewhere")
   return a.parse_args()
 
 
@@ -26,7 +28,7 @@ def stage_0(args):
   with open(args.file, "r") as f:
     data = json.load(f)
 
-  ydl = yt_dlp.YoutubeDL()
+  ydl = yt_dlp.YoutubeDL() if yt_dlp is not None else None
 
   entries = []
   if os.path.exists(args.cache_file):
@@ -47,7 +49,6 @@ def stage_0(args):
       i += 1
       continue
     i += 1
-    print(text)
 
     if not is_yt:
       entries.append(entry)
@@ -72,7 +73,39 @@ def stage_0(args):
   with open(args.cache_file, "w") as c:
     json.dump(entries, c, indent=2)
 
+
   return
+def stage_1(args):
+  with open(args.cache_file, "r") as c:
+    entries = json.load(c)
+  if args.markwhen is not None:
+    markwhen(entries, args)
+
+def markwhen(entries, args):
+  print(f"Making timeline for {len(entries)} entries")
+  f = open(args.markwhen, "w")
+  for e in entries:
+    time = datetime\
+      .fromtimestamp(int(e["timestamp"]) / 1000, UTC)\
+      .strftime('%Y-%m-%d %H:%M')
+    url = e['url']
+    if 'title' in e:
+      title = e['title']
+      title = title.replace("[", "「")
+      title = title.replace("]", "」")
+      f.write(f"{time}: [{title}]({url})\n")
+    else:
+      f.write(f"{time}: [{url}]({url})\n")
+  f.close()
+
+  assert(not os.system(f"mw {args.markwhen} yt_timeline.html"))
+
+  with open("yt_timeline.html", "r") as f:
+    data = f.read()
+  data = data.replace("Markwhen Timeline", "Music Timeline")
+
+  with open("yt_timeline.html", "w") as f:
+    f.write(data)
 
 
 if __name__ == "__main__": main()
